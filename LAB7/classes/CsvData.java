39ee5ecd4dcd403986e577827da30df1f7b89961
Daniel Lazureanu <87951869+daniellazureanu@users.noreply.github.com>
2024-01-07 22:52:17 +0200

    thread

---
+++ LAB7/classes/CsvData.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class CsvData implements Comparable<CsvData>{
+    String category;
+    String region;
+    int year;
+    String unit;
+    double value;
+
+    public CsvData(String category, String region, int year, String unit, double value) {
+        this.category = category;
+        this.region = region;
+        this.year = year;
+        this.unit = unit;
+        this.value = value;
+    }
+
+    public int compareTo(CsvData other){
+        return Integer.compare(this.year, other.year);
+    }
+
+    @Override
+    public String toString() {
+        return  "category=" + category +
+                ", region=" + region +
+                ", year=" + year +
+                ", unit=" + unit +
+                ", value=" + value;
+    }
+}
---
+++ LAB7/classes/TemaCsvThread.java
@@ -0,0 +1,66 @@
+package classes;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TemaCsvThread implements Runnable {
+    private String filePath;
+
+    public TemaCsvThread(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void run() {
+        List<CsvData> dataList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                CsvData csvData = new CsvData(
+                        values[0].trim(),
+                        values[1].trim(),
+                        Integer.parseInt(values[2].trim()),
+                        values[3].trim(),
+                        Double.parseDouble(values[4].trim())
+                );
+
+                dataList.add(csvData);
+
+                // Normal output
+                System.out.println(csvData);
+            }
+
+            // Sorted output by year field
+            Collections.sort(dataList);
+
+            for (CsvData data : dataList) {
+                System.out.println(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "classes/forest_surfaces.csv";
+        TemaCsvThread temaCsv = new TemaCsvThread(filePath);
+
+        // Creăm un fir de execuție și îl pornim
+        Thread thread = new Thread(temaCsv);
+        thread.start();
+
+        try {
+            // Așteptăm firul de execuție să se termine
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
